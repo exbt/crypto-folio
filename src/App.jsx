83b33e8d09365f2a8528367d62f6dcb3e3a2976d
exbt <batuhankurnaz34@gmail.com>
2025-12-27 18:16:42 +0300

    initial commit for CryptoFolio project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen text-white flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-500 underline">
+        Hello CryptoFolio! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
